54d7e2a779618a026965bd912c47b7a1941085a7
Tijmenk <93768063+SnepCnep@users.noreply.github.com>
2023-01-03 23:53:50 +0100

    Update server.lua

--- sc-allwipe/server/server.lua
+++ sc-allwipe/server/server.lua
@@ -1,152 +1,80 @@
-ESX = exports["es_extended"]:getSharedObject() -- Es_extended 1.18+
+ESX = exports["es_extended"]:getSharedObject()
+-- ^^ nieuwe esx
+--ESX = nil TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+-- ^^ oude esx
+
+
+
 --
--- -- Gemaakt door 𝓢𝓷𝓮𝓹𝓒𝓷𝓮𝓹#0074 | script nakken ben je de lul =) 
+-- -- Gemaakt door 𝓢𝓷𝓮𝓹𝓒𝓷𝓮𝓹#0074 =) 
 --
 
-Citizen.CreateThread(function()
-print('Instelleren van belangerijke content (Er kan mogelijk een lagspike komen)')
-        Citizen.Wait(2500)    
-print('Gelukt, u kunt nu gebruik maken van de AllWipe')
+RegisterCommand("clearallmenu", function(source, args)
+    local src = source
+    local xPlayer = ESX.GetPlayerFromId(src)
+if (source > 0) then
+    if IsPlayerAceAllowed(source, "sc.npccontroll") then
+            TriggerClientEvent('sc:clearallmenu-open', src)
+	else
+			TriggerClientEvent('okokNotify:Alert', src, "SC ClearAll", "Onjuisten permissions.", 2000, 'error')
+    end
+else
+	print("SC - Dit command kan niet via de console worden uitgevoerd")
+end
 end)
 
-local cooldownmenu = true
-local cooldown = true
-local time = 5
-local timee = 3
-Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(1000)
-		if cooldownmenu then
+local allow = false
+RegisterServerEvent('SC-wipesyteem')
+AddEventHandler('SC-wipesyteem', function(sort, protect)
+	local toDelete = {}
+        if protect == 1243 then
+            allow = true
 		else
-			time = time - 1
-			if time <= 0 then
-                time = 5
-                    cooldownmenu = true
-			end
+            allow = false
+            DropPlayer(source, "SC-Protect - U probeert AllWipe activeren")
 		end
-		if cooldown then
-		else
-			timee = timee - 1
-			if timee <= 0 then
-                timee = 3
-                    cooldown = true
+        if allow then
+			if sort == "cars" then
+				toDelete = GetAllVehicles()
+                TriggerClientEvent("txAdmin:receiveAnnounce", -1, "Alle CARS zijn verwijdert", "SC Allwipe")
+			elseif sort == "peds" then
+				toDelete = GetAllPeds()
+                TriggerClientEvent("txAdmin:receiveAnnounce", -1, "Alle PEDS zijn verwijdert", "SC Allwipe")
+			elseif sort == "props" then
+				toDelete = GetAllObjects()
+                TriggerClientEvent("txAdmin:receiveAnnounce", -1, "Alle PROPS zijn verwijdert", "SC Allwipe")
 			end
-		end
+	for _,entity in pairs(toDelete) do
+		DeleteEntity(entity)
+                allow = false
 	end
-end)
-
-
--- clearallmenu register
-RegisterNetEvent('sc:clearallmenu-open')
-AddEventHandler('sc:clearallmenu-open', function()
-        if cooldownmenu then
-            clearallmenu()
-            cooldownmenu = false
-		else
-			exports['okokNotify']:Alert("SC ClearAll", "Wacht tot de cooldown weg is.", 1500, 'error')
-		end
-end)
-
-
--- clearallmenu
-local toDelete = {}
-function clearallmenu()  
-
-    local Elements = {
-        {label = "<----| SC ClearAll |---->", name = "niks"},
-        {label = "Auto's", name = "cars"},
-        {label = "Auto's | Not in vehicle", name = "carsdo"},
-        {label = "Objecten", name = "props"},
-        {label = "Peds", name = "peds"},
-	{label = "All", name = "all"},
-        {label = "<----| SC AllWipe |---->", name = "niks"},
-      }
-    
-
-      ESX.UI.Menu.Open("default", GetCurrentResourceName(), "clearallmenu", {
-        title = "SC ClearAll", 
-        align    = 'top-right',
-        elements = Elements
-      }, function(data,menu)
-           if cooldown then
-                    cooldown = false
-                local protect = 1243 -- nou hacker dit is het wachtwoord success    = ) 
-        	if data.current.name == "niks" then
-                	print("SC-ALlWipe ( Gemaakt door SnepCnep )") -- dit doet niks dit is er zodat er geen errors komen 
-
-            elseif data.current.name == "cars" then
-				TriggerServerEvent('SC-wipesyteem', "cars", protect)
-                    print("Jij delete alle voeruigen")
-
-            elseif data.current.name == "carsdo" then
-    for vehicle in EnumerateVehicles() do
-        if (not IsPedAPlayer(GetPedInVehicleSeat(vehicle, -1))) then 
-            SetVehicleHasBeenOwnedByPlayer(vehicle, false) 
-            SetEntityAsMissionEntity(vehicle, false, false) 
-            DeleteVehicle(vehicle)
-            if (DoesEntityExist(vehicle)) then 
-                DeleteVehicle(vehicle) 
             end
-        end
-    end
+end)
 
-            elseif data.current.name == "props" then
-				TriggerServerEvent('SC-wipesyteem', "props", protect)
-                    print("Jij delete alle props")
-
-			elseif data.current.name == 'peds' then
-				TriggerServerEvent('SC-wipesyteem', "peds", protect)
-                    print("Jij delete alle peds")
-
-            elseif data.current.name == 'all' then
-				TriggerServerEvent('SC-wipesyteem-all', protect)
-                    print("Jij delete alles")
-        end
-				else
-		exports['okokNotify']:Alert("SC ClearAll", "Wacht tot de cooldown weg is.", 1500, 'error')
-				end
-      end, function(data, menu) 
-        menu.close()
-      end)
-    end
-
-
-
-
---- function nog iets
-local entityEnumerator = {
-  __gc = function(enum)
-    if enum.destructor and enum.handle then
-      enum.destructor(enum.handle)
-    end
-    enum.destructor = nil
-    enum.handle = nil
-  end
-}
-
-local function EnumerateEntities(initFunc, moveFunc, disposeFunc)
-  return coroutine.wrap(function()
-    local iter, id = initFunc()
-    if not id or id == 0 then
-      disposeFunc(iter)
-      return
-    end
-    
-    local enum = {handle = iter, destructor = disposeFunc}
-    setmetatable(enum, entityEnumerator)
-    
-    local next = true
-    repeat
-      coroutine.yield(id)
-      next, id = moveFunc(iter)
-    until not next
-    
-    enum.destructor, enum.handle = nil, nil
-    disposeFunc(iter)
-  end)
-end
-
-
-function EnumerateVehicles()
-  return EnumerateEntities(FindFirstVehicle, FindNextVehicle, EndFindVehicle)
-end
+RegisterServerEvent('SC-wipesyteem-all')
+AddEventHandler('SC-wipesyteem-all', function(protect)
+local toDelete1 = GetAllVehicles()
+local toDelete2 = GetAllPeds()
+local toDelete3 = GetAllObjects()
+        if protect == 1243 then
+            allow = true
+		else
+            allow = false
+            DropPlayer(source, "SC-Protect - U probeert AllWipe activeren")
+		end
+        if allow then
+			TriggerClientEvent("txAdmin:receiveAnnounce", -1, "Alle (cars/props/peds) zijn verwijdert", "SC Allwipe")
+	for _,entity1 in pairs(toDelete1) do
+		DeleteEntity(entity1)
+		allow = false
+	end
+	for _,entity2 in pairs(toDelete2) do
+		DeleteEntity(entity2)
+		allow = false
+	end
+	for _,entity3 in pairs(toDelete3) do
+		DeleteEntity(entity3)
+       	allow = false
+	end
+            end
+end)
