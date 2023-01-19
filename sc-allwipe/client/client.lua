276ea948fb085c244a72fc6c6d306dcc34e7bb45
SnepCnep <93768063+SnepCnep@users.noreply.github.com>
2023-01-19 22:14:10 +0100

    Update client.lua

--- sc-allwipe/client/client.lua
+++ sc-allwipe/client/client.lua
@@ -1,12 +1,17 @@
-ESX = exports["es_extended"]:getSharedObject() -- Es_extended 1.18+
+ESX = exports["es_extended"]:getSharedObject()
+-- ^^ nieuwe esx
+--ESX = nil TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+-- ^^ oude esx
+
+
 --
 -- -- Gemaakt door 𝓢𝓷𝓮𝓹𝓒𝓷𝓮𝓹#0074 | script nakken ben je de lul =) 
 --
 
 Citizen.CreateThread(function()
-print('Instelleren van belangerijke content (Er kan mogelijk een lagspike komen)')
+	print('Instelleren van belangerijke content (Er kan mogelijk een lagspike komen)')
         Citizen.Wait(2500)    
-print('Gelukt, u kunt nu gebruik maken van de AllWipe')
+	print('Gelukt, u kunt nu gebruik maken van de AllWipe')
 end)
 
 local cooldownmenu = true
@@ -21,7 +26,7 @@ Citizen.CreateThread(function()
 			time = time - 1
 			if time <= 0 then
                 time = 5
-                    cooldownmenu = true
+                cooldownmenu = true
 			end
 		end
 		if cooldown then
@@ -53,29 +58,35 @@ local toDelete = {}
 function clearallmenu()  
 
     local Elements = {
-        {label = "<----| SC ClearAll |---->", name = "niks"},
+        {label = "<----| ATH ClearAll |---->", name = "niks"},
         {label = "Auto's", name = "cars"},
+        {label = "Auto's | Not in vehicle", name = "carsdo"},
         {label = "Objecten", name = "props"},
         {label = "Peds", name = "peds"},
 		{label = "All", name = "all"},
-        {label = "<----| SC ClearAll |---->", name = "niks"},
+        {label = "<----| ATH AllWipe |---->", name = "niks"},
       }
     
 
       ESX.UI.Menu.Open("default", GetCurrentResourceName(), "clearallmenu", {
         title = "SC ClearAll", 
-        align    = 'top-right',
+        align    = "top-right",
         elements = Elements
       }, function(data,menu)
            if cooldown then
                     cooldown = false
-                local protect = 1243 -- nou hacker dit is het wachtwoord success    = ) 
+                local protect = 1232131312313313132 -- nou hacker dit is het wachtwoord success    = ) 
         	if data.current.name == "niks" then
                 	print("SC-ALlWipe ( Gemaakt door SnepCnep )") -- dit doet niks dit is er zodat er geen errors komen 
 
             elseif data.current.name == "cars" then
 				TriggerServerEvent('SC-wipesyteem', "cars", protect)
                     print("Jij delete alle voeruigen")
+
+            elseif data.current.name == "carsdo" then
+                    print("Alle voeruigen waar niemand in zit zijn verwijdert.")
+                    TriggerServerEvent('SC-wipesyteem-melding', protect)
+                    clearautos()
 
             elseif data.current.name == "props" then
 				TriggerServerEvent('SC-wipesyteem', "props", protect)
@@ -97,3 +108,54 @@ function clearallmenu()
       end)
     end
 
+
+--- function nog iets
+local entityEnumerator = {
+  __gc = function(enum)
+    if enum.destructor and enum.handle then
+      enum.destructor(enum.handle)
+    end
+    enum.destructor = nil
+    enum.handle = nil
+  end
+}
+
+local function EnumerateEntities(initFunc, moveFunc, disposeFunc)
+  return coroutine.wrap(function()
+    local iter, id = initFunc()
+    if not id or id == 0 then
+      disposeFunc(iter)
+      return
+    end
+    
+    local enum = {handle = iter, destructor = disposeFunc}
+    setmetatable(enum, entityEnumerator)
+    
+    local next = true
+    repeat
+      coroutine.yield(id)
+      next, id = moveFunc(iter)
+    until not next
+    
+    enum.destructor, enum.handle = nil, nil
+    disposeFunc(iter)
+  end)
+end
+
+
+function EnumerateVehicles()
+  return EnumerateEntities(FindFirstVehicle, FindNextVehicle, EndFindVehicle)
+end
+
+function clearautos() 
+    for vehicle in EnumerateVehicles() do
+        if (not IsPedAPlayer(GetPedInVehicleSeat(vehicle, -1))) then 
+            SetVehicleHasBeenOwnedByPlayer(vehicle, false) 
+            SetEntityAsMissionEntity(vehicle, false, false) 
+            DeleteVehicle(vehicle)
+            if (DoesEntityExist(vehicle)) then 
+                DeleteVehicle(vehicle) 
+            end
+        end
+    end
+end
